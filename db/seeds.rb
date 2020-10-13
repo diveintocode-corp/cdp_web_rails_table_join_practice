f26c3977f648dbc982520a5a8153eb5c80beafde
mem <mem0112@hotmail.com>
2020-10-13 23:26:38 +0900

    コメント削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,25 +11,8 @@ user1 = User.create({name: "Alice"})
 user2 = User.create({name: "Bob"})
 user3 = User.create({name: "Cameron"})
 
-# Bookのtitle: RubyBook, RailsGuide, SQLGuide
-# book1 = Book.create({title: "RubyBook"})
-# book2 = Book.create({title: "RailsGuide"})
-# book3 = Book.create({title: "SQLGuide"})
-
-# Variationのkind: PDF, paper book
-# variation1 = Variation.create({kind: "PDF"})
-# variation2 = Variation.create({kind: "paper book"})
-
 user1.books.build(title: "RubyBook").variations.build(kind: "paper book")
 user1.books.build(title: "RailsGuide").variations.build(kind: "PDF")
 user2.books.build(title: "RailsGuide").variations.build(kind: "PDF")
 user1.save
 user2.save
-# user.articles.build(title: "My article")
-
-# book1.user = user1
-# book2.user = user1
-# book2.user = user2
-
-# variation1.book << book2
-# variation2.book << book1
